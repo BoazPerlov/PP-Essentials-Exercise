1ffabd1df8a79306ba3abc11156d316415d3247c
Boaz Perlov <boaz@perlov.co.uk>
2022-09-07 22:29:05 +0100

    Add files via upload

---
+++ sentenceCheckerTestUnit.py
@@ -0,0 +1,22 @@
+import unittest
+from validSentenceChecker import sentenceChecker
+
+#using the unittest library, we create a subclass of its TestCase class to check several example of valid and invalid sentences
+class TestChecker(unittest.TestCase):
+
+    def test_sentence(self):
+        self.assertTrue(sentenceChecker('The quick brown fox said “hello Mr lazy dog”.'))
+        self.assertTrue(sentenceChecker('The quick brown fox said hello Mr lazy dog.'))
+        self.assertTrue(sentenceChecker('One lazy dog is too few, 13 is too many.'))
+        self.assertTrue(sentenceChecker('One lazy dog is too few, thirteen is too many.'))
+        self.assertTrue(sentenceChecker('How many "lazy dogs" are there?'))
+        self.assertFalse(sentenceChecker('The quick brown fox said "hello Mr. lazy dog".'))
+        self.assertFalse(sentenceChecker('the quick brown fox said “hello Mr lazy dog".'))
+        self.assertFalse(sentenceChecker('"The quick brown fox said “hello Mr lazy dog."'))
+        self.assertFalse(sentenceChecker('One lazy dog is too few, 12 is too many.'))
+        self.assertFalse(sentenceChecker('Are there 11, 12, or 13 lazy dogs?'))
+        self.assertFalse(sentenceChecker('There is no punctuation in this sentence'))
+
+
+if __name__ == "__main__":
+    unittest.main()
