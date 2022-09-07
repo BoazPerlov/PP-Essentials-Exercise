e2738482ce8bfd1be5d778d1168ecf624a7993f1
Boaz Perlov <boaz@perlov.co.uk>
2022-09-07 22:45:33 +0100

    Update validSentenceChecker.py

--- validSentenceChecker.py
+++ validSentenceChecker.py
@@ -8,8 +8,9 @@
 import re #regular expressions python library
 import os
 
-def sentenceChcker(checkedString):
-    #The main function creates an empty list and appends all the above function tests to it as booleans. If any of the items on the list are false, user is informed sentnece is invalid.
+def sentenceChecker(checkedString):
+    #The main function creates an empty list and appends all the above function tests to it as booleans. 
+    # If any of the items on the list are false, function returns false, otherwise true.
     testsList = []
     if checkedString: #Making sure the sting isn't empty
         testsList.append(True if re.search("^[A-Z]",checkedString) else False) #Using reguar expressions to test if string starts with capital letter
@@ -18,9 +19,9 @@ def sentenceChcker(checkedString):
         testsList.append(not re.findall(r"\.", checkedString[:-1])) #Using regular expressions to test if there are any '.' in all but the last character of the string.
         testsList.append(not any(int(num) < 13 for num in re.findall(r'\d+', checkedString))) #using the re.findall method to test the string for any numbers, then return false if any are below 13
         if testsList.count(False) > 0: #if none of the above tests are false, user gets notification that their string is valid, otherwise notified invalid
-            print('Sentence is invalid')
+            return False
         else:
-            print('Sentence is valid!')
+            return True
     else:
         print('Empty sentence submitted, please try again.')
 
@@ -29,8 +30,15 @@ if __name__ == "__main__":
     while looper == True:
         os.system('CLS') #clears the console, keeping it nice and clean for the user
         inputSentence = input('Please submit the sentence you wish to check here: ') #user input
-        sentenceChcker(inputSentence) #run the checker function
-        while True:    
+        if sentenceChecker(inputSentence): #run the checker function
+            print('Sentence is valid!')
+        else:
+            print('Sentence is invalid')
+        while True: 
+            #using an additional while loop as I need to look for three states of the input- 'y', 'n' and any other character. 
+            #When the user select 'n' for not wanting to continue, the looper variable turns false and the nested while loop break, causing the function to stop
+            #a 'y' answer breaks the nested while loop but not the outer while loop, continuing the sentence checking
+            #any other character gives the user an error message and asks them again
             continueTest = input('Would you like to check additional sentences? y/n: ') #checking if user wants to continue
             if continueTest == 'n':
                 os.system('CLS')
@@ -41,8 +49,3 @@ if __name__ == "__main__":
             else:
                 print('Invalid character submitted, please try again!')
                 continue
-
-
-
-
-
